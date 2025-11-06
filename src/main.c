64d90233ab23766e07a443933daa7b5ef6e157a0
Mridula Naikawadi <mnaikawa@purdue.edu>
2025-11-05 18:40:54 -0800

    can play tones on speaker

--- src/main.c
+++ src/main.c
@@ -1,56 +1,61 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 #include "pico/stdlib.h"
-#include "hardware/irq.h"
 #include "hardware/pwm.h"
-#include "hardware/adc.h"
-#include "hardware/dma.h"
+
+#define GPIO 28
+
+#define C4 262
+#define D4 294
+#define E4 330
+#define F4 349
+#define G4 392
+#define A4 440
+#define B4 494
+#define C5 523
 
 //////////////////////////////////////////////////////////////////////////////
 
-// const char* username1 = "sewell7";
-// const char* username2 = "";
-// const char* username3 = "";
-// const char* username4 = "";
+void play_tone(uint gpio, int freq, int duration_ms) {
+    uint slice_num = pwm_gpio_to_slice_num(gpio);
+    uint channel = pwm_gpio_to_channel(gpio);
 
-//////////////////////////////////////////////////////////////////////////////
+    // calculate clkdiv to keep wrap <= 65535 = 2^16
+    float clkdiv = (float)125000000 / (freq * 65535.0);
+    if (clkdiv < 1.0f) clkdiv = 1.0f; // 1 is the minimum allowed
 
-// Function Declarations
-int determine_gpio_from_channel(int);
-void init_adc(int);
-void init_adc_for_freerun();
-void modify_frequency(int, float);
+    pwm_set_clkdiv(slice_num, clkdiv);
+    pwm_set_wrap(slice_num, 65535);
+    pwm_set_chan_level(slice_num, channel, 32768); // 50% duty
 
-//////////////////////////////////////////////////////////////////////////////
-
-// Global Variable Declarations
-// int step0 = 0;
-// int offset0 = 0;
-// int step1 = 0;
-// int offset1 = 0;
+    pwm_set_enabled(slice_num, true);
+    sleep_ms(duration_ms); // so note plays intended length
+    pwm_set_chan_level(slice_num, channel, 0);
+}
 
 //////////////////////////////////////////////////////////////////////////////
 
 int main() {
-    //initialize all
     stdio_init_all();
+    gpio_set_function(GPIO, GPIO_FUNC_PWM);
 
-    //initialize adc
-    float pwm_freq = 0;
-    int adc_channel = 5;
-    init_adc(adc_channel);
-    init_adc_for_freerun();
+    // Twinkle Twinkle Little Star
+    struct {
+        int freq;
+        int duration;
+    } song[] = {
+        {C4, 500}, {C4, 500}, {G4, 500}, {G4, 500},
+        {A4, 500}, {A4, 500}, {G4, 1000},
+        {F4, 500}, {F4, 500}, {E4, 500}, {E4, 500},
+        {D4, 500}, {D4, 500}, {C4, 1000}
+    };
 
-    //initialize pwm
-    int pwm_channel = 0;
+    int song_length = sizeof(song)/sizeof(song[0]);
 
-    //freerunning output
     for(;;) {
-        pwm_freq = (adc_hw->result) / 7.0;
-        modify_frequency(pwm_channel, pwm_freq);
-        fflush(stdout);
-        sleep_ms(1000);
+        for (int i = 0; i < song_length; i++) {
+            play_tone(GPIO, song[i].freq, song[i].duration);
+            sleep_ms(50);
+        }
     }
+
+    return 0;
 }

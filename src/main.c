e3640e457261a4167ae2c02c609af4bfe77bd727
Mridula Naikawadi <mnaikawa@purdue.edu>
2025-11-12 22:03:02 -0800

    changed song to ievan polkka

--- src/main.c
+++ src/main.c
@@ -1,16 +1,21 @@
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
+// #include "pico/ff_stdio.h"
 
 #define GPIO 28
 
-#define C4 262
-#define D4 294
-#define E4 330
-#define F4 349
-#define G4 392
-#define A4 440
-#define B4 494
-#define C5 523
+#define C 262
+#define Csharp 277
+#define D 294
+#define E 330
+#define F 349
+#define Fsharp 370
+#define G 392
+#define Gsharp 415
+#define A 440
+#define B 494
+#define C2 523
+#define C2sharp 554
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -37,15 +42,32 @@ int main() {
     stdio_init_all();
     gpio_set_function(GPIO, GPIO_FUNC_PWM);
 
-    // Twinkle Twinkle Little Star
+    // ievan polkka
     struct {
         int freq;
         int duration;
     } song[] = {
-        {C4, 500}, {C4, 500}, {G4, 500}, {G4, 500},
-        {A4, 500}, {A4, 500}, {G4, 1000},
-        {F4, 500}, {F4, 500}, {E4, 500}, {E4, 500},
-        {D4, 500}, {D4, 500}, {C4, 1000}
+        {Csharp, 500}, {Fsharp, 500}, {Fsharp, 750}, {Gsharp, 250}, 
+        {A, 250}, {A, 250}, {Fsharp, 250}, {Fsharp, 250},
+        {Fsharp, 500}, {Fsharp, 250}, {A, 250}, {Gsharp, 500},
+        {E, 500}, {E, 500}, {Gsharp, 500}, {A, 500},
+        {Fsharp, 500}, {Fsharp, 500}, {Fsharp, 250}, {Fsharp, 250},
+        {Csharp, 500}, {Fsharp, 500}, {Fsharp, 750}, {Gsharp, 250},
+        {A, 500}, {Fsharp, 500}, {Fsharp, 500}, {Fsharp, 250},
+        {A, 250}, {C2sharp, 250}, {C2sharp, 250}, {C2sharp, 250},
+        {B, 250}, {A, 500}, {Gsharp, 500}, {A, 500},
+        {Fsharp, 500}, {Fsharp, 500}, {Fsharp, 250}, {A, 250},
+        {C2sharp, 500}, {C2sharp, 500}, {B, 500}, {A, 500},
+        {Gsharp, 500}, {E, 500}, {E, 500}, {E, 250},
+        {Gsharp, 250}, {B, 250}, {B, 250}, {B, 250},
+        {B, 250}, {A, 250}, {A, 250}, {Gsharp, 250},
+        {Gsharp, 250}, {A, 500}, {Fsharp, 500}, {Fsharp, 500},
+        {Fsharp, 250}, {A, 250}, {C2sharp, 500}, {C2sharp, 500}, 
+        {B, 500}, {A, 500}, {Gsharp, 500}, {E, 500}, 
+        {E, 500}, {E, 250}, {Gsharp, 250}, {B, 250}, 
+        {B, 250}, {B, 250}, {B, 250}, {A, 250}, 
+        {A, 250}, {Gsharp, 250}, {Gsharp, 250}, {A, 500}, 
+        {Fsharp, 500}, {Fsharp, 750}, {Fsharp, 250}
     };
 
     int song_length = sizeof(song)/sizeof(song[0]);
